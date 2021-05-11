65875e76174c567510f0a068a5fa071236853908
89104775491Ds <dan-sivod@mail.ru>
2021-05-11 21:20:19 +0300

    Создание таблицы и конфигурации Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private Util util = new Util();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -13,6 +20,29 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            String query = "CREATE TABLE IF NOT EXISTS users(" +
+                    "id integer primary key auto_increment, " +
+                    "name varchar(100), " +
+                    "lastName varchar(100), " +
+                    "age int);";
+
+            session.createSQLQuery(query);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
